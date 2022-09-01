230b979864458d4bbb1815a3f6cd109ddb29ca36
geraldosd <juninhosh2013sh@gmail.com>
2022-09-01 13:29:45 -0300

    fist

---
+++ includes/header.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="<?=base_url()?>">
+            <img src="<?=base_url()?>assets/images/l.png" class="img-fluid" style="max-height: 80px">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link" href="<?=base_url()?>">Inicio</a>
+                <a class="nav-link" href="<?=base_url()?>sobre">Sobre</a>
+                <a class="nav-link" href="<?=base_url()?>servicos">Serviços</a>
+                <a class="nav-link" href="<?=base_url()?>atendimento-ao-cliente">Atendimento ao
+                    Cliente</a>
+            </div>
+           
+        </div>
+     
+    <div >
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup" >
+            <div class="navbar-nav">
+                <a class="nav-link" href="<?=base_url()?>login">Login</a>
+            </div>
+        </div>   
+    </div> 
+</nav>
